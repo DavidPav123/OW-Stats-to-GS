d8f18c5853c370d99fd220d33eb8589939485a45
DavidPav <you@example.com>
2022-08-26 20:27:20 -1000

    Fixed formatting

--- stats_to_GS.py
+++ stats_to_GS.py
@@ -7,20 +7,22 @@ from typing import NoReturn
 
 from google_sheets_push import update_sheet
 
-
+#List of pages for google sheets to write to
 pages_to_update: str = [
-    "Placeholder",
-    "Placeholder2",
-    "Sheet1",
-    "Sheet2",
-    "Sheet3",
-    "Sheet4",
-    "Sheet5",
+    "Placeholder", #DO NOT DELETE
+    "Placeholder2", #DO NOT DELETE
+    "Sheet1", #Change to page 1 
+    "Sheet2", #Change to page 2
+    "Sheet3", #Change to page 3  
+    "Sheet4", #Change to page 4
+    "Sheet5", #Chnage to page 5
 ]
+#Variable for switching pages when new file is detected
 current_page: int = 0
-# Range to update
+# Range to update in sheets change infor after
 range_name: str = f"{pages_to_update[current_page]}!A1:Z26"
-cur_map = ""
+#Name of the current map
+cur_map: str = ""
 
 
 def get_latest_file() -> str:
@@ -94,7 +96,7 @@ def file_len(file_to_read: str) -> int:
         return lines
 
 
-def update_page(page_list: list, current_page):
+def update_page(page_list: list, current_page: int):
     return f"{page_list[current_page]}!A1:Z26"
 
 
